ae4a65ffd2c680e3108bf7a66fc5e51cf063edce
Tushar Porje <tusharporje13091999@gmail.com>
2024-05-31 22:59:51 +0530

    preorder and postorder solution

---
+++ Trees/PreOrderTraversal.java
@@ -0,0 +1,33 @@
+package Trees;
+
+/*  
+        4
+    3       5
+2      1  6     7
+
+*/
+
+public class PreOrderTraversal {
+    public static void main(String[] args) {
+        Tree root=new Tree(4);
+        root.left=new Tree(3);
+        root.right=new Tree(5);
+        root.left.left=new Tree(2);
+        root.left.right=new Tree(1);
+        root.right.left=new Tree(6);
+        root.right.right=new Tree(7);
+        
+        solution(root);
+        
+    }
+
+    public static void solution(Tree root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data);
+        solution(root.left);
+        solution(root.right);
+
+    }
+}
