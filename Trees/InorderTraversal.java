1a3f0d60049c620485efaf92ea5784b8abe87757
Tushar Porje <tusharporje13091999@gmail.com>
2024-05-31 22:53:25 +0530

    inorderTraversal

---
+++ Trees/InorderTraversal.java
@@ -0,0 +1,35 @@
+package Trees;
+
+/*  
+        4
+    3       5
+2      1  6     7
+
+*/
+
+// output = 2 3 1 4 6 5 7
+
+public class InorderTraversal {
+    public static void main(String[] args) {
+        Tree root=new Tree(4);
+        root.left=new Tree(3);
+        root.right=new Tree(5);
+        root.left.left=new Tree(2);
+        root.left.right=new Tree(1);
+        root.right.left=new Tree(6);
+        root.right.right=new Tree(7);
+        
+        solution(root);
+        
+    }
+
+    public static void solution(Tree root){
+        if(root==null){
+            return;
+        }
+        solution(root.left);
+        System.out.println(root.data);
+        solution(root.right);
+
+    }
+}
---
+++ Trees/Tree.java
@@ -0,0 +1,29 @@
+package Trees;
+
+public class Tree {
+    int data;
+    Tree left;
+    Tree right;
+    
+    public Tree() {
+        this.data=-1;
+        this.left=null;
+        this.right=null;
+    }
+
+    public Tree(int data) {
+        this();
+        this.data = data;
+    }
+
+    public Tree(int data, Tree left, Tree right) {
+        this(data);
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Tree [data=" + data + ", left=" + left + ", right=" + right + "]";
+    }
+}
